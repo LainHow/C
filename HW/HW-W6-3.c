128b03f2447129bc261a5002b70e2852eb41b045
LainHow <01053145@email.ntou.edu.tw>
2021-11-04 16:49:30 +0800

    1

--- HW/HW-W6-3.c
+++ HW/HW-W6-3.c
@@ -3,10 +3,17 @@
 int main()
 {
     char a[1000]={0} , b[10][10][10]={0};
-    float M_I[10][10]={0},c;
+    float M[10][10]={0},c;
     int line=0,ele=0, row=0, i=0,j=0,k=0,l=0;
     gets(a);
-    
+    /*line17-37
+    把輸入字串(a[k])轉成矩陣(b[i][j][k])
+    k遞增
+    遇到','j就+1,
+    遇到';'i就+1,
+    沒遇到','跟';'
+    就將a[k]讀入b[i][j][l],l+1
+    */
     for ( int k = 0; k < sizeof(a); k++)
     {
         if (a[k] == ',')
@@ -28,14 +35,20 @@ int main()
         b[i][j][l]=a[k];
         l++;
     }
+    /*line41-48
+    把b陣列中的字元轉成浮點數存入M_I
+    */
     row= (ele / line );
     for (int i = 0; i < line; i++)
     {
         for (int j = 0; j < row; j++)
         {
-            M_I[i][j] = atof(b[i][j]);
+            M[i][j] = atof(b[i][j]);
         }
     }
+    /*line52-73
+    將M轉為擴展矩陣
+    */
     if (row != line)
     {
         printf("此矩陣無反矩陣");
@@ -49,64 +62,76 @@ int main()
             {
                 if (i==j)
                 {
-                    M_I[i][line+j]=1.0;
+                    M[i][line+j]=1.0;
                 }
                 else
                 {
-                    M_I[i][line+j]=0.0;
+                    M[i][line+j]=0.0;
                 }    
             }            
         }        
     }
-    /*M_I目前為擴展矩陣*/
+    /*line81-95
+    將M變成上三角矩陣                                                            j=1 -> c=4/1=4                              j=2 -> c=7/1=7
+    若要將每一列的第一項都變為0(除了第1列)                  1 2 3 ->i=0            1 2 3               1  2  3               1  2  3           1   2   3
+    將第一列設為指定列(i)                                  4 5 6       目標列[1]->4 5 6            -> 0 -3 -6               0 -3 -6           0  -3  -6
+    對於目標列先找到該列的倍數c = M[j][i] / M[i][i]        7 8 9                  7 8 9               7  8  9     目標列[2]->7  8  9        -> 0  -6 -12 
+    目標列中第k個元素(M[j][k])減去 指定列第k個元素(M[i][k])*c                                                                            
+    */
     for (int i = 0; i < line; i++)
     {
         for (j = 1; j < line; j++)
         {
-            if (M_I[i][i] == 0)
+            if (M[i][i] == 0)
             {
                 break;
             }
-            c = M_I[i+1][i] / M_I[i][i];
+            c = M[i+1][i] / M[i][i];
             for ( k = 0; k < line*2; k++)
             {
-                M_I[j][k] = M_I[j][k] - M_I[i][k]*c;
+                M[j][k] = M[j][k] - M[i][k]*c;
             }
         }
     }
-    /*M_I目前為上三角矩陣*/
+    /*line99-110
+    從最後一列依序往上減
+    */
     for ( i = line-1; i > 0; i--)
     {
         for (int j = i-1; j >= 0; j--)
         {            
-            c=M_I[j][j+1] / M_I[i][i];
-            for (int k = line*2-1; k >=0; k--)
+            c=M[j][j+1] / M[i][i];
+            for (int k = line * 2 - 1; k >= 0; k--)
             {
-                M_I[j][k] = M_I[j][k]-M_I[i][k]*c;
+                M[j][k] = M[j][k] - M[i][k]*c;
                 
             }
         }
     }
+    /*line115-122
+    將第i列的每個元素除以c(M[i][i])
+    讓M[i][i]變回1
+    */
     for (int i = 0; i < line; i++)
     {
-        c=M_I[i][i];
-        for (int j = 0; j < line*2; j++)
+        c = M[i][i];
+        for (int j = 0; j < line * 2; j++)
         {
-            M_I[i][j] = M_I[i][j] / c;
+            M[i][j] = M[i][j] / c;
         }
-        
     }
+    /*輸出矩陣*/
     for (int i = 0; i < line; i++)
     {
-        for (int j = line; j < line*2; j++)
+        for (int j = line; j < line * 2; j++)
         {
             if (j == line)
             {
-                printf("\n%.1f\t", M_I[i][j]);
+                printf("\n%.1f\t", M[i][j]);
             }
             else
             {
-                printf("%.1f\t", M_I[i][j]);
+                printf("%.1f\t", M[i][j]);
             }
         }
     }
--- HW/test.c
+++ HW/test.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void input_to_matrix(char a[1000]){
+void input_to_matrix(char a[10][10][1000]){
+    char a[10][10][1000]={'0'};
     int i = 0,j = 0,k = 0,l = 0,line = 0,element = 0 , row = 0;
-    float b[10][10][10]={0},M[10][10]={0};
+    float M[10][10]={0};
     for ( k = 0; k < sizeof(a); k++)
     {
         if (a[k] == ',')
@@ -22,27 +23,34 @@ void input_to_matrix(char a[1000]){
             element++;
             continue;
         }        
-        b[i][j][l]=a[k];
+        a[i][j][l]=a[0][0][k];
         l++;
     }
-    row = element / line ;
-    for (int i = 0; i < line; i++)
-    {
-        for (int j = 0; j < row; j++)
-        {
-            M[i][j] = atof(b[i][j]);
-        }
-    }
-    return M;
 }
 
 int main()
 {
-    char a[1000]={0}, b[1000]={0};
+    char a[10][10][1000]={0}, b[1000]={0};
     float M_1[10][10],M_2[10][10]={0}, M_product[10][10]={0};
     int line1=0,ele1=0,line2=0,ele2=0, row1=0,row2=0, i1=0,j1=0,k1=0,l1=0 ,i2=0,j2=0,k2=0,l2=0;
     gets(a);
-    gets(b);
+    input_to_matrix(a);
+    for (int i = 0; i < sizeof(a[0]); i++)
+    {
+        for (int j = 0; j < sizeof(a[0]); j++)
+        {
+            M_1[i][j] = atof(a[i][j]);
+        }
+    }
+
+    for (int i = 0; i < sizeof(a[0]); i++)
+    {
+        for (int j = 0; j < sizeof(a[0]); j++)
+        {
+            printf("%.1f\t",M_1[i][j]);
+        }
+        printf("\n");
+    }
     
     return 0;
 }
