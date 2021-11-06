9ddc1679af1fd52a1c3df2975a9bc14e9d29d3b1
LainHow <01053145@email.ntou.edu.tw>
2021-11-06 15:18:55 +0800

    20211106

--- HW/HW-W7/HW-W7-1.c
+++ HW/HW-W7/HW-W7-1.c
@@ -1,20 +1,26 @@
-#include <stdio.h>
-#include <string.h>
+#include<stdio.h>
+#include <stdlib.h>
 
 
-int main(char *passwd[], int move)
+int main(int argc, char *argv[])
 {
-    char encrypted[100];
-    int i,j,k,t;
+    char passwd[100];
+    int i,j,k,t,move;
+    for (int i = 0; i < argc-2; i++)
+    {
+        passwd[i] = *argv[i+1];
+    }
+    move = atoi(argv[argc-1]);
+
     for(i=0; i < 5; i++)
         {
-            if(*passwd[i] >= 'A' && passwd[i] <= 'Z')
+            if(passwd[i] >= 'A' && passwd[i] <= 'Z')
             {
-                *passwd[i] = ((*passwd[i]-'A')+move)%26+'A';
+                passwd[i] = ((passwd[i]-'A')+move)%26+'A';
             }
-            else if(*passwd[i] >= 'a' && *passwd[i] <= 'z')
+            else if(passwd[i] >= 'a' && passwd[i] <= 'z')
             {
-                *passwd[i] = ((*passwd[i]-'a')+move)%26+'a';
+                passwd[i] = ((passwd[i]-'a')+move)%26+'a';
             }
         }
         printf("%s",passwd);
