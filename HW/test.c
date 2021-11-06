792aa51ba720fafba080d3930beb4b86678dbd32
LainHow <01053145@email.ntou.edu.tw>
2021-11-06 15:22:41 +0800

    20211106

--- HW/test.c
+++ HW/test.c
@@ -3,32 +3,7 @@
 #include <stdlib.h>
 
 int main(){
-    char a[2][2][5];
-    char str[4]="abc";
-
-    for (int i = 0; i < 2; i++)
-    {
-        for (int j = 0; j < 2; j++)
-        {
-            strcat(a[i][j] ,str);
-        }
-        
-    }
-    for (int i = 0; i < 2; i++)
-    {
-        for (int j = 0; j < 2; j++)
-        {
-            printf("%s\n",a[i][j]);
-        }
-        
-    }
     
-    
-    
-    
-   
-    
-
 }
 
 
